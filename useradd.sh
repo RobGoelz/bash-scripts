36343c0606e5c5867933deb354c35d422ebc9d05
Rob Goelz <RobGoelz@gmail.com>
2016-03-28 20:25:24 +0000

    Add conditional logic to check for root privileges at beginning of script.

--- useradd.sh
+++ useradd.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ `id -u` != 0 ]; then
+	echo "This script must be run with root privileges!"
+	exit 1
+fi
+
 echo "Type the name of the user you want to add, followed by [Enter]"
 
 read user
@@ -13,3 +18,4 @@ echo "Set password for $user"
 passwd $user
 
 echo "User addition completed."
+
