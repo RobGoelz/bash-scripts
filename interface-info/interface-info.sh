666addc4b4699ebbe00ceb67b7df618827ec979f
Rob Goelz <RobGoelz@gmail.com>
2016-04-11 13:23:41 -0400

    Update interface-info header to include more information about the script.

--- interface-info/interface-info.sh
+++ interface-info/interface-info.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Script to capture interface information for servers
 
+# This script takes in server names as positional parameters 
+#+ to bash, and returns all interface information 
+#+ (via ifconfig) and the route information for the server.
+
+# This information is then logged using a code block redirect
+#+ , to the local directory where the script was run.
+
+
 # Declaring variables:
 ROOT_UID=0
 E_NOTROOT=87
